3e65e6d41801ca3ab23282a1c8ef221bf9bd39b0
RockstArjun <chauhanarjun064@gmail.com>
2021-05-07 10:00:40 +0530

    small project

--- P3_snake_water_gun game.py
+++ P3_snake_water_gun game.py
@@ -1,1 +1,12 @@
-print("cg")
+import random
+import time
+print("snake")
+time.sleep(2)
+print("water")
+time.sleep(2)
+print("gun")
+print("we have a ---")
+time.sleep(4)
+lis=["snake","water","gun"]
+num=random.choice(lis)
+print(num)
