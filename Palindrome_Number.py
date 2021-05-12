436b313993bb915a0c680d249cd322b19b070e41
RockstArjun <chauhanarjun064@gmail.com>
2021-05-12 23:53:57 +0530

    Small-Project

--- Palindrome_Number.py
+++ Palindrome_Number.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     numbers = []
     for i in range(1,n+1):
         number = int(input(f"Enter the {i} number:- "))
-        numbers.append(number)
+        numbers.append(number)  
 
     for i in range(n):
         print(f"Next palindrome for {numbers[i]} is {next_palindrome(numbers[i])}")
