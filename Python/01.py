c5fa6c11831ea92abeb927ddb1c5abc2ea7e99b1
RockstArjun <chauhanarjun064@gmail.com>
2021-10-04 11:40:50 -0700

    hello

---
+++ Python/01.py
@@ -0,0 +1,11 @@
+# Name:- Arjun Chauhan
+# Instagram handle:- @python.programe
+
+def reg():
+    a=input()
+    b=input()
+    if a.__len__() == b.__len__():
+        print("true")
+    else:
+        print("false")
+reg()
